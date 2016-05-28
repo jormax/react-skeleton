a5c7bae24ea9744f4c52694f5b03648af583357c
JorMax <yes0324@gmail.com>
2016-05-29 04:34:28 +0800

    reusable

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 var React = require('react');
 var ReactDOM= require('react-dom');
-var List = require('./components/List.jsx');
+var ListManager =require('./components/ListManager.jsx');
 
-ReactDOM.render(<List />,document.getElementById('ingredients'));
+ReactDOM.render(<ListManager title="Ingredients" headingColor="#028090"/>, document.getElementById('ingredients'));
+ReactDOM.render(<ListManager title="toDo" headingColor="#00A896"/>, document.getElementById('todo'));
+ReactDOM.render(<ListManager title="Xmas" headingColor="#05668D"/>, document.getElementById('Xmas'));
+ReactDOM.render(<ListManager title="Watever" headingColor="#d90429"/>, document.getElementById('Watever'));
